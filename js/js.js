bab7ad47421f4be9deaa51754bf19a9f3f32d23a
Pritam Das <dasp8708@gmail.com>
2020-02-07 20:19:45 +0530

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,1 +1,16 @@
-
+$(document).ready(function(){
+	$("#header, .info").ripples({
+		dropRadius: 20,
+		perturbance: 0.0,
+	});
+	
+	$('.parent-container').magnificPopup({
+		delegate: 'a',//child item selector, by clicking on it popup will open
+		type: 'image',
+		gallery:{
+			enabled: true
+		}
+	});
+	
+	
+});
